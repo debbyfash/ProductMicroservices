b010b4c2dac9c0a6df900075bf92b3cf1bbeabe3
Faseun Deborah <58981883+debbyfash@users.noreply.github.com>
2021-10-28 10:20:36 +0100

    Add project files.

---
+++ ProductMicroservice/Repository/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ProductMicroservice.DBContext;
+using ProductMicroservice.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroservice.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _db;
+
+        public ProductRepository(ProductContext db)
+        {
+            _db = db;
+        }
+        public void DeleteProduct(int productId)
+        {
+            var product = _db.Products.Find(productId);
+            _db.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int productId)
+        {
+            return _db.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _db.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _db.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
